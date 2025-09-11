60a6924a752ca4fe715e02127f8085a54e9b2b30
ZZJ <166818908+ZZJ-jack@users.noreply.github.com>
2025-09-11 11:53:39 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/ZZJ-jack",
+  home: "https://zzjjack.us.kg",
+  email: "mailto:zzjjack@zzjjack.us.kg",
 };
 </script>
 
